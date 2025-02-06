eb80869ee665941b108276b06767287310dfdf1c
drinnnird-uoft <drin.brown@alum.utoronto.ca>
2025-02-06 16:34:40 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJpbm5pcmQiLCJhIjoiY201b2RyYXRhMGt1YTJvcHQ4ZjU4dDYycSJ9.jHNRKSu149-F5s157m1GwA'; // Add default public map token from your Mapbox account
+const map = new mapboxgl.Map({
+container: 'map-container', // map container ID
+style: 'mapbox://styles/drinnird/cm6if66vx008601qq12b6ggk6', // style URL
+center: [..., ...], // starting position [lng, lat]
+zoom:
+});
+
+map.on('load', () => {
+
+    // Add a data source containing GeoJSON data
+    map.addSource('uoft-data', {
+        type: 'geojson',
+        data: {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {
+                        "name": "Sidney Smith Hall"
+                    },
+                    "geometry": {
+                        "coordinates": [
+                            -79.39865237301687,
+                            43.662343395037766
+                        ],
+                        "type": "Point"
+                    }
+                }
+            ]
+        }
+    });
+
+    map.addLayer({
+        'id': 'uoft-pnt',
+        'type': 'circle',
+        'source': 'uoft-data',
+        'paint': {
+            'circle-radius': 6,
+            'circle-color': '#B42222'
+        }
+
+    });
+
+});
